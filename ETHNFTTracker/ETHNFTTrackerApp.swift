e7b643215350c4474df2047925c0fec1ef333087
Jan Sallads <jan@fddb.de>
2022-11-10 02:38:03 +0100

    Initial Commit

---
+++ ETHNFTTracker/ETHNFTTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETHNFTTrackerApp.swift
+//  ETHNFTTracker
+//
+//  Created by Jan on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ETHNFTTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
